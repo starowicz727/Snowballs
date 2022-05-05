f03c7f433d0cbbe646f3c3a61c3df69532596d3e
starowicz727 <starowicz727@gmail.com>
2022-05-05 22:12:54 +0200

    destroy prizes at random time

---
+++ Assets/DestroyAtRandomTime.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAtRandomTime : MonoBehaviour
+{
+    public int minTime, maxTime;
+    private float destroyT; //po jakim czasie ma nast¹piæ usuniêcie obiektu
+    private float destroyTimer = 0f;//odliczanie czasu do usuniêcia
+    void Start()
+    {
+        destroyT = Random.RandomRange(minTime, maxTime+1);
+    }
+
+    void Update()
+    {
+        destroyTimer += Time.deltaTime;
+        if(destroyTimer >= destroyT)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
