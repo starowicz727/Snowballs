7cb9fcca293c71696eb28e0bc8b1dd4eaaba998b
starowicz727 <starowicz727@gmail.com>
2022-05-05 17:00:49 +0200

    snowball destroy

--- Assets/Snowball.cs
+++ Assets/Snowball.cs
@@ -5,11 +5,29 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class Snowball : MonoBehaviour
 {
-    private float speed = 10; 
-
+    private float speed = 10;
+    private bool alreadyThrown;
+    private float timer;
+    private void Start()
+    {
+        alreadyThrown = false;
+        timer = 0f;
+    }
+    private void Update()
+    {
+        if (alreadyThrown)
+        {
+            timer += Time.deltaTime;
+            if (timer >= 5)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
     public void Throw()
     {
+        alreadyThrown = true;
         this.gameObject.GetComponent<XRGrabInteractable>().enabled = false;
-        this.gameObject.GetComponent<Rigidbody>().velocity = speed*this.gameObject.transform.forward;
+        this.gameObject.GetComponent<Rigidbody>().velocity = speed * this.gameObject.transform.forward;
     }
 }
